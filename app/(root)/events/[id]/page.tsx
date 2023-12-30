25f71ae485d141993b8a81bfaa295f55c3828fb6
sundayuchegbu <sundayuchegbu10@gmail.com>
2023-12-30 05:32:15 +0100

    fix error with webhook

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -35,7 +35,7 @@ const EventDetails = async ({
           <div className="flex w-full flex-col gap-8 p-5 md:p-10">
             <div className="flex flex-col gap-6">
               <h2 className="h2-bold">{event.title}</h2>
-              <div className="flex flex-col gap-3 sm:flex-row sm:flex-row sm:items-center">
+              <div className="flex flex-col gap-3  sm:flex-row sm:items-center">
                 <div className="flex gap-3">
                   <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
                     {event.isFree ? 'FREE' : `$${event.price}`}
