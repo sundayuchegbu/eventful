e63c1acaec2654e5a2cc323f515c0340457c5291
sundayuchegbu <sundayuchegbu10@gmail.com>
2024-11-14 21:54:03 +0100

    change button commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,18 +1,18 @@
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { Button } from '../ui/button';
-import NavItems from './NavItems';
-import MobileNav from './MobileNav';
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
-        <Link href={'/'} className="w-36">
+        <Link href={"/"} className="w-36">
           <Image
-            src="/assets/images/logo.svg"
+            src="/assets/images/logo.png"
             width={128}
             height={38}
             alt="eventful logo"
@@ -22,15 +22,15 @@ const Header = () => {
           <nav className="md:flex-between hidden w-full max-w-xs">
             <NavItems />
           </nav>
-        </SignedIn>{' '}
+        </SignedIn>{" "}
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
             <MobileNav />
           </SignedIn>
           <SignedOut>
-            <Button asChild className="rounded-full " size={'lg'}>
-              <Link href={'/sign-in'}>Login</Link>
+            <Button asChild className="rounded-full " size={"lg"}>
+              <Link href={"/sign-in"}>Login</Link>
             </Button>
           </SignedOut>
         </div>
