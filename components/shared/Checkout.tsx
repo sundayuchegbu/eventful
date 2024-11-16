9cdac6c56320ea2d2c06d27194a995ed39f83cf2
sundayuchegbu <sundayuchegbu10@gmail.com>
2024-11-16 12:34:59 +0100

    fixed stripe error

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,8 +1,8 @@
-import { IEvent } from '@/lib/database/models/event.model';
-import React, { useEffect } from 'react';
-import { Button } from '../ui/button';
-import { loadStripe } from '@stripe/stripe-js';
-import { checkoutOrder } from '@/lib/actions/order.actions';
+import { IEvent } from "@/lib/database/models/event.model";
+import React, { useEffect } from "react";
+import { Button } from "../ui/button";
+import { loadStripe } from "@stripe/stripe-js";
+import { checkoutOrder } from "@/lib/actions/order.actions";
 
 loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
@@ -10,13 +10,13 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
   useEffect(() => {
     // Check to see if this is a redirect back from Checkout
     const query = new URLSearchParams(window.location.search);
-    if (query.get('success')) {
-      console.log('Order placed! You will receive an email confirmation.');
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
     }
 
-    if (query.get('canceled')) {
+    if (query.get("canceled")) {
       console.log(
-        'Order canceled -- continue to shop around and checkout when you’re ready.'
+        "Order canceled -- continue to shop around and checkout when you’re ready."
       );
     }
   }, []);
@@ -34,8 +34,8 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
 
   return (
     <form action={onCheckout} method="post">
-      <Button type="submit" role="link" size={'lg'} className="button sm:w-fit">
-        {event.isFree ? 'Get Ticket' : 'Buy Ticket'}
+      <Button type="submit" role="link" size={"lg"} className="button sm:w-fit">
+        {event.isFree ? "Get Ticket" : "Buy Ticket"}
       </Button>
     </form>
   );
